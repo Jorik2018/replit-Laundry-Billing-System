449dfcea1e4c211cc6c0199d27d41843d27706a9
Erik Antonio Alarcón Pinedo <C8VMM@ferrovial.com>
2026-08-07 13:50:57 -0500

    fix: align pnpm version for docker build

--- artifacts/lavanderia/src/main.tsx
+++ artifacts/lavanderia/src/main.tsx
@@ -5,6 +5,5 @@ import App from './App';
 import './index.css';
 import { setBaseUrl } from '@workspace/api-client-react';
 
-alert('VITE_API_URL='+import.meta.env.VITE_API_URL+';BASE_URL=>'+import.meta.env.BASE_URL)
 setBaseUrl(import.meta.env.VITE_API_URL.replace(/\/$/, ''));
 createRoot(document.getElementById('root')!).render(<App />);
